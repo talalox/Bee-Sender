37ecd635dc701347e82fad75fa234f02bdb04c69
talalox <104396676+talalox@users.noreply.github.com>
2022-04-25 22:12:09 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,4 +2,4 @@
 // change this address to your own address , a half transfer fee will send to this address. 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-var FeeReciever = "0xEAc8555E83919Cedc065849b0FDc843bC03EFd4d";
+var FeeReciever = "0x224440B0b5deB02a9c609f91B1EC70c869F65d48";
